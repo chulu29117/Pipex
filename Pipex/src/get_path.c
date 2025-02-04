c729e60fc79f9e73c6b38a461a2034a12bfa77c2
clu <clu@student.hive.fi>
2025-02-04 23:46:06 +0200

    stable version but fixing errors for pipexMedic

--- Pipex/src/get_path.c
+++ Pipex/src/get_path.c
@@ -6,7 +6,7 @@
 /*   By: clu <clu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 10:10:16 by clu               #+#    #+#             */
-/*   Updated: 2025/02/04 15:49:33 by clu              ###   ########.fr       */
+/*   Updated: 2025/02/04 23:45:33 by clu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static char **get_path_dirs(char **envp)
 	int i;
 	char **paths;
 
+	if (!envp || !*envp)
+		return (NULL);
 	i = 0;
 	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5))
 		i++;
@@ -25,6 +27,71 @@ static char **get_path_dirs(char **envp)
 	paths = ft_split(envp[i] + 5, ':');
 	return (paths);
 }
+
+// char *find_path(char *cmd, char **envp)
+// {
+// 	char	**paths;
+// 	char	*full_path;
+// 	char	*temp;
+// 	int		i;
+// 	static char *default_paths[] = {"/bin", "/usr/bin", NULL};
+
+// 	// If envp is NULL or empty, check absolute paths or common directories
+// 	if (!envp || !*envp || !get_path_dirs(envp))
+// 	{
+// 		if (cmd[0] == '/' || cmd[0] == '.')
+// 		{
+// 			full_path = ft_strdup(cmd);
+// 			if (!full_path)
+// 				pipex_error("pipex: memory allocation failed");
+// 			return (full_path);
+// 		}
+// 		// Check manually in /bin and /usr/bin
+// 		i = 0;
+// 		while (default_paths[i])
+// 		{
+// 			temp = ft_strjoin(default_paths[i], "/");
+// 			if (!temp)
+// 				pipex_error("pipex: memory allocation failed");
+// 			full_path = ft_strjoin(temp, cmd);
+// 			free(temp);
+// 			if (!full_path)
+// 				pipex_error("pipex: memory allocation failed");
+// 			if (access(full_path, X_OK) == 0)
+// 				return (full_path);
+// 			free(full_path);
+// 			i++;
+// 		}
+// 		return (NULL);
+// 	}
+
+// 	// Normal PATH handling
+// 	paths = get_path_dirs(envp);
+// 	if (!paths)
+// 		return (NULL);
+// 	i = 0;
+// 	while (paths[i])
+// 	{
+// 		temp = ft_strjoin(paths[i], "/");
+// 		if (!temp)
+// 			pipex_error("pipex: memory allocation failed");
+// 		full_path = ft_strjoin(temp, cmd);
+// 		free(temp);
+// 		if (!full_path)
+// 			pipex_error("pipex: memory allocation failed");
+// 		if (access(full_path, X_OK) == 0)
+// 		{
+// 			ft_free_array(paths);
+// 			return (full_path);
+// 		}
+// 		free(full_path);
+// 		i++;
+// 	}
+// 	ft_free_array(paths);
+// 	return (NULL);
+// }
+
+
 
 char *find_path(char *cmd, char **envp)
 {
--- Pipex/src/init_process.c
+++ Pipex/src/init_process.c
@@ -6,7 +6,7 @@
 /*   By: clu <clu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 18:34:59 by clu               #+#    #+#             */
-/*   Updated: 2025/02/04 15:34:50 by clu              ###   ########.fr       */
+/*   Updated: 2025/02/04 23:21:46 by clu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,12 @@ void	init_pipex(t_pipex *pipex, char **argv, char **envp)
 {
 	pipex->infile = open(argv[1], O_RDONLY);
 	if (pipex->infile < 0)
-		perror("pipex: infile error");
+	{
+		perror("pipex: line 1: input");
+		pipex->infile = open("/dev/null", O_RDONLY);
+		if (pipex->infile < 0)
+			pipex_error("pipex: failed to open /dev/null");
+	}
 	pipex->outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (pipex->outfile < 0)
 		pipex_error("pipex: outfile error");
@@ -26,4 +31,19 @@ void	init_pipex(t_pipex *pipex, char **argv, char **envp)
 	if (pipe(pipex->pipe_fds) == -1)
 		pipex_error("pipex: pipe creation failed");
 }
+
+// void	init_pipex(t_pipex *pipex, char **argv, char **envp)
+// {
+// 	pipex->infile = open(argv[1], O_RDONLY);
+// 	if (pipex->infile < 0)
+// 		perror("pipex: infile error");
+// 	pipex->outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 	if (pipex->outfile < 0)
+// 		pipex_error("pipex: outfile error");
+// 	pipex->cmd1 = argv[2];
+// 	pipex->cmd2 = argv[3];
+// 	pipex->envp = envp;
+// 	if (pipe(pipex->pipe_fds) == -1)
+// 		pipex_error("pipex: pipe creation failed");
+// }
 
